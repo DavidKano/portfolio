1780450a287f17d26c1794acbb9f329d4e21e4e0
David Cano Jaramillo <137452183+DavidKano@users.noreply.github.com>
2025-07-28 12:42:24 +0200

    Publicación del portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,astro,ts,tsx,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
